d1e49c6a900f42ad4ba657735e7ea31189a7415e
Valid Block <88240972+validblock@users.noreply.github.com>
2022-01-16 19:04:36 +0100

    1.0.0 (#1)
    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Pageres = require('pageres');
+
+(async () => {
+    await new Pageres({
+        delay: 2,
+        hide: ['.welcome', '.menu', '.feature', '#bottombuttons'],
+        darkMode: true,
+        filename: '<%= date %>'
+    })
+        .src('https://xn--strungsauskunft-9sb.de/stromausfall', ['1024x1280'])
+        .dest(`${__dirname}/${(new Date()).getFullYear()}`)
+        .run();
+
+    console.log('Finished!');
+})();

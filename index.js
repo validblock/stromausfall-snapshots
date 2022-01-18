9eff29e156ce2fbf9fb288ee3c103661b06fb0c6
Shadowy Super Coder <88240972+validblock@users.noreply.github.com>
2022-01-18 14:50:14 +0100

    Refactor core app

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const Pageres = require('pageres');
+const fs = require('fs');
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const localeDate = currentDate.toLocaleDateString('fr-CA');
+const filePath = `${__dirname}/${currentYear}`;
+const fileSuffix = '.png';
+
+const normalizedFileName = () => {
+    let count = 1;
+
+    while (count < 10) {
+        const fileName = `${localeDate} (${count})`;
+
+        if (fs.existsSync(`${filePath}/${fileName}${fileSuffix}`)) {
+            count ++;
+        } else {
+            return fileName;
+        }
+    }
+}
 
 (async () => {
+    const path = `${__dirname}/${(new Date()).getFullYear()}`;
+
     await new Pageres({
         delay: 20,
-        incrementalName: true,
         hide: ['.welcome', '.menu', '.feature', '#bottombuttons'],
         darkMode: true,
-        filename: '<%= date %>'
+        filename: normalizedFileName()
     })
         .src('https://xn--strungsauskunft-9sb.de/stromausfall', ['1024x1280'])
-        .dest(`${__dirname}/${(new Date()).getFullYear()}`)
+        .dest(filePath)
         .run();
 
     console.log('Finished!');
 })();
+
